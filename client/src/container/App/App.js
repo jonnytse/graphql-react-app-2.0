955ab05598d4e3fd9d5340c28276b07110694969
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-10-01 16:54:04 +0100

    moved core information from index.js into app.js

--- client/src/container/App/App.js
+++ client/src/container/App/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
 
 function App() {
   return (
@@ -8,5 +10,25 @@ function App() {
     </div>
   );
 }
+
+const client = new ApolloClient({
+  uri: 'https://fakerql.nplan.io/graphql',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      {
+        allUsers{
+          firstName
+          lastName
+          id
+          email
+        }
+      }  
+    `
+  })
+  .then(result => console.log(result));
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,27 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App/App';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { gql } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://fakerql.nplan.io/graphql',
-  cache: new InMemoryCache()
-});
-
-client
-  .query({
-    query: gql`
-      {
-        allUsers{
-          firstName
-          lastName
-          id
-        }
-      }  
-    `
-  })
-  .then(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
